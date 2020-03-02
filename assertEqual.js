348a7ba93413089fbe86888aaa9a6c6af64fec94
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-02 20:51:45 +0000

    funtion to tell us when an assertion function has passed

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "Bye");
+assertEqual(4,4);
+assertEqual(3, 1);
