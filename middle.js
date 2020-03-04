443604bb520eb46bf34684a15a90769f960dc829
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-04 19:11:56 +0000

    a program to remove unwanted numbers from an array

--- middle.js
+++ middle.js
@@ -1,30 +1,25 @@
 const middle = function(array) {
-  let midElements = [];
+  const midElements = [];
   const midIndex = Math.floor(array.length/2);
-  if (array.length === 1 || array.length === 2) {
-    return midElements;
-  } else if (array.length % 2 !== 0) {
+  if (array.length % 2 !== 0) {
     midElements.push(array[midIndex]);
-    return midElements;
-  } else {
+  } else if(array.length > 0) {
     midElements.push(array[midIndex - 1]);
     midElements.push(array[midIndex]);
-    return midElements;
   }
+  return midElements;
 }
 
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
-      
       return false;
     }
   }
   return true;
 }
 
-const assertArraysEqual = function(array, expected) {
-  actual = middle(array);
+const assertArraysEqual = function(actual, expected) {
   const isEqual = eqArrays(actual, expected);
   if (isEqual) { 
     console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅: ${actual} === ${expected}\n`);
@@ -35,7 +30,7 @@ const assertArraysEqual = function(array, expected) {
 
 
 
-assertArraysEqual([1, 2, 3], [2]);
-assertArraysEqual([1, 2, 3, 4, 5], [3]);
-assertArraysEqual([1, 2, 3, 4], [2, 3]); // => [2, 3]
-assertArraysEqual([1, 2, 3, 4, 5, 6], [3, 4]); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
---
+++ without.js
@@ -0,0 +1,33 @@
+const without = function(sourceArray, itemsToRemoveArray) {
+  const newArray = [];
+  for (let originalNumbers of sourceArray) {
+    for (let [unwantedNumberIndex, unwantedNumbers] of itemsToRemoveArray.entries()) {
+      if (originalNumbers !== unwantedNumbers && unwantedNumberIndex === (itemsToRemoveArray.length - 1)) {
+        newArray.push(originalNumbers);
+      } 
+    }
+  }
+  console.log("outside newArray is ", newArray);
+  return newArray;
+}
+
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  const isEqual = eqArrays(actual, expected);
+  if (isEqual) { 
+    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅: ${actual} === ${expected}\n`);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 ${actual} !== ${expected}\n`);
+  }
+}
+
+assertArraysEqual(without([1,2,3], [1]), [2,3]);  
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
