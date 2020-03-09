83b3fc4ed6daddb48a8c3a55b020e27748f44681
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-09 20:14:19 +0000

    implemented mocha chai testing libraries

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,35 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
+describe('#tail', () => {
+  describe('testing with input ---> ["Yo yo", "Lighthouse", "Labs"]', () => {
+    it('should return 2 for test1.length', () => {
+      assert.strictEqual(tail(["Yo yo", "Lighthouse", "Labs"]).length, 2);
+    });
 
-const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
-assertEqual(test1.length, 2);
-assertEqual(test1[0], "Lighthouse");
-assertEqual(test1[1], "Labs");
-const emptyArray = [];
-assertEqual(tail(emptyArray).length, 0);
+    it('should return "Lighthouse for test1[0]', () => {
+      const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
+      assert.strictEqual(test1[0], 'Lighthouse');
+    });
+
+    it('should return "Labs for test1[1]', () => {
+      const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
+      assert.strictEqual(test1[1], 'Labs');
+    });
+  });
+   
+  describe('testing with input ---> []', () => {
+    it('should return undefined for []', () => {
+      const emptyArray = tail([]);
+      assert.strictEqual(emptyArray.length, 0);
+    })
+  })
+});
+
+// const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
+// assertEqual(test1.length, 2);
+// assertEqual(test1[0], "Lighthouse");
+// assertEqual(test1[1], "Labs");
+// const emptyArray = [];
+// assertEqual(tail(emptyArray).length, 0);
 
