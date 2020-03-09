0ff507f436fb14fc46f36f89a7b204c803d81a70
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-09 19:31:08 +0000

    Created a test file to refactor middle file

--- middle.js
+++ middle.js
@@ -10,29 +10,4 @@ const middle = function(array) {
   return midElements;
 }
 
-const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray.length !== secondArray.length) {
-      return false
-    } else if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-  if (isEqual) { 
-    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅: ${actual} === ${expected}\n`);
-  } else {
-    console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 ${actual} !== ${expected}\n`);
-  }
-}
-
-
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
