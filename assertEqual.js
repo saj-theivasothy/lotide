4aed37a7209ece41f398e686638f826c6cf59dc5
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-09 18:31:47 +0000

    refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -6,10 +6,7 @@
     }
   };
 
+  module.exports = assertEqual;
   // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual("Hello", "Hello");
-  assertEqual("Hello", "Bye");
-  assertEqual(4,4);
-  assertEqual(3, 1);
+  
+  
--- head.js
+++ head.js
@@ -7,17 +7,4 @@ const head = function(array) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([1]),1);
-assertEqual(head([]),1);
-assertEqual(head([]), undefined);
+module.exports = head;
