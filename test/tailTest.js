de257778461b9101b118763744ac9060406ae647
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-09 20:21:45 +0000

    refactored code

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,27 +2,13 @@ const tail = require('../tail');
 const assert = require('chai').assert;
 
 describe('#tail', () => {
-  describe('testing with input ---> ["Yo yo", "Lighthouse", "Labs"]', () => {
-    it('should return 2 for test1.length', () => {
-      assert.strictEqual(tail(["Yo yo", "Lighthouse", "Labs"]).length, 2);
-    });
-
-    it('should return "Lighthouse for test1[0]', () => {
-      const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
-      assert.strictEqual(test1[0], 'Lighthouse');
-    });
-
-    it('should return "Labs for test1[1]', () => {
-      const test1 = tail(["Yo yo", "Lighthouse", "Labs"]);
-      assert.strictEqual(test1[1], 'Labs');
-    });
+  it('should return ["Lighthouse", "Labs"] for ["Yo yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
-   
-  describe('testing with input ---> []', () => {
-    it('should return undefined for []', () => {
-      const emptyArray = tail([]);
-      assert.strictEqual(emptyArray.length, 0);
-    })
+
+  it('should return undefined for []', () => {
+    const emptyArray = tail([]);
+    assert.strictEqual(emptyArray.length, 0);
   })
 });
 
